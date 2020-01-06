60a9d76e92a2da63cfcf61d862aac2f3ff73b6d5
Monika <mm@froso.de>
2020-01-06 11:38:34 +0100

    finished

--- Tannenbaum/README.md
+++ Tannenbaum/README.md
@@ -1,2 +1,14 @@
-# xmas-tree
+# Christmas-tree
+
 A small C# project that prints a Christmas tree as an console output.
+
+### How to start?
+
+    1. Open the project in your IDE.
+    
+    2. Run the main.cs file.
+
+Recommended size of your terminal: 80 x 25
+
+Questions? Write: mm@froso.de
+
--- Tannenbaum/Run.cs
+++ Tannenbaum/Run.cs
@@ -5,9 +5,10 @@
 //Beschreibung:
 //Aenderungen:      17.12. Start of development
 //                  21.12. String duplication method implemented
+//
+//
 
 using System;
-using System.Linq;
 
 namespace Tannenbaum
 {
@@ -17,91 +18,105 @@ namespace Tannenbaum
         {
             //Zentrale Daten definieren
             string input = "";
-            string mode = "";
-            int decision = 1;
+            string input2 = "";
+            string decision = "";
+            int mode = 1;
             int height = 1;
 
             //lokalen Daten
-            bool weiter = true;
+            bool isRunning = true;
 
             //Hauptschleife
-            while (weiter)
+            while (isRunning)
             {
                 //1. Höhe bestimmen
                 bool done = false;
                 do
                 {
-                    // todo: beenden
-                    Console.WriteLine("Geben Sie bitte die Höhe an:");
+                    Console.WriteLine("Wenn Sie das Programm beenden wollen, schreiben Sie q, sonst schreiben Sie n:");
+                    decision = Console.ReadLine();
+
+                    Console.WriteLine("Geben Sie bitte die Höhe an (1-22):");
                     input = Console.ReadLine();
                     height = Convert.ToInt32(input);
 
-                    if (height >= 1)
+                    if (decision == "q")
                     {
-                        if (height <= 22)
+                        isRunning = false;
+                    }
+                    else if (decision == "n")
+                    {
+                        if (height >= 1)
                         {
-                            done = true;
-
-                            Console.WriteLine(
-                                "Wenn Sie den traditionellen Weihnachtsbaum sehen wollen, schreiben Sie bitte 1");
-                            Console.WriteLine(
-                                "Wenn Sie einen umgekehrten Weihnachtsbaum sehen wollen, schreiben Sie bitte 2");
-
-                            mode = Console.ReadLine();
-                            decision = Convert.ToInt32(mode);
-
-                            if (decision == 1)
+                            if (height <= 22)
                             {
-                                for (int i = 1; i <= height; i++)
+                                done = true;
+
+                                Console.WriteLine(
+                                    "Wenn Sie den traditionellen Weihnachtsbaum sehen wollen, schreiben Sie bitte 1:");
+                                Console.WriteLine(
+                                    "Wenn Sie einen umgekehrten Weihnachtsbaum sehen wollen, schreiben Sie bitte 2:");
+
+                                input2 = Console.ReadLine();
+                                mode = Convert.ToInt32(input2);
+
+                                if (mode == 1)
                                 {
-                                    for (int j = Console.WindowWidth / 2 - (i - 1); j > 0; j--)
+                                    for (int i = 1; i <= height; i++)
+                                    {
+                                        for (int j = Console.WindowWidth / 2 - (i - 1); j > 0; j--)
+                                        {
+                                            Console.Write(" ");
+                                        }
+
+                                        Console.Write(new string('\u2605', 2 * i - 1));
+                                        Console.WriteLine();
+                                    }
+
+                                    for (int i = Console.WindowWidth / 2; i > 0; i--)
+                                    {
+                                        Console.Write(" ");
+                                    }
+                                    Console.Write('\u2605');
+                                    Console.WriteLine();
+                                    for (int i = Console.WindowWidth / 2; i > 0; i--)
+                                    {
+                                        Console.Write(" ");
+                                    }
+                                    Console.Write('\u2605');
+                                    Console.WriteLine();
+                                }
+                                else if (mode == 2)
+                                {
+                                    for (int i = Console.WindowWidth / 2; i > 0; i--)
                                     {
                                         Console.Write(" ");
                                     }
 
-                                    Console.Write(new string('\u2605', 2 * i - 1));
+                                    Console.Write('\u2605');
                                     Console.WriteLine();
-                                }
-
-                                for (int i = Console.WindowWidth / 2; i > 0; i--)
-                                {
-                                    Console.Write(" ");
-                                }
-
-                                Console.Write('\u2605');
-                                Console.WriteLine();
-                                for (int i = Console.WindowWidth / 2; i > 0; i--)
-                                {
-                                    Console.Write(" ");
-                                }
-
-                                Console.Write('\u2605');
-                                Console.WriteLine();
-                            }
-                            else if (decision == 2)
-                            {
-                                for (int i = Console.WindowWidth / 2; i > 0; i--)
-                                {
-                                    Console.Write(" ");
-                                }
-
-                                Console.Write('\u2605');
-                                Console.WriteLine();
-                                for (int i = Console.WindowWidth / 2; i > 0; i--)
-                                {
-                                    Console.Write(" ");
-                                }
-
-                                Console.Write('\u2605');
-                                for (int i = height; i > 0; i--)
-                                {
-                                    for (int j = Console.WindowWidth / 2 - (i - 1); j > 0; j--)
+                                    for (int i = Console.WindowWidth / 2; i > 0; i--)
                                     {
                                         Console.Write(" ");
                                     }
-
-                                    Console.Write(new string('\u2605', 2 * i - 1));
+                                    Console.Write('\u2605');
                                     Console.WriteLine();
+
+                                    for (int i = height; i > 0; i--)
+                                    {
+                                        for (int j = Console.WindowWidth / 2 - (i - 1); j > 0; j--)
+                                        {
+                                            Console.Write(" ");
+                                        }
+
+                                        Console.Write(new string('\u2605', 2 * i - 1));
+                                        Console.WriteLine();
+                                    }
+                                }
+                                else
+                                {
+                                    done = false;
+                                    Console.WriteLine("Ungultiger Wert. Geben Sie bitte ein zahl zwischen 1 und 22.");
                                 }
                             }
                             else
@@ -115,11 +130,6 @@ namespace Tannenbaum
                             done = false;
                             Console.WriteLine("Ungultiger Wert. Geben Sie bitte ein zahl zwischen 1 und 22.");
                         }
-                    }
-                    else
-                    {
-                        done = false;
-                        Console.WriteLine("Ungultiger Wert. Geben Sie bitte ein zahl zwischen 1 und 22.");
                     }
                 } while (!done);
             }
